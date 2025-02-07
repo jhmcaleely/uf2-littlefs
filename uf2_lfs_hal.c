b2f99c02d661504df8a32b4c375562678e600017
John McAleely <john@mcaleely.com>
2025-02-07 23:26:51 +0000

    reorganise and refactor

--- uf2_lfs_hal.c
+++ uf2_lfs_hal.c
@@ -6,7 +6,7 @@
 #include "uf2/uf2.h"
 
 #define PICO_UF2_FAMILYID 0xe48bff56
-#define DEVICE_BLOCK_COUNT (PICO_FLASH_SIZE_BYTES / PICO_ERASE_PAGE_SIZE)
+#define PICO_DEVICE_BLOCK_COUNT (PICO_FLASH_SIZE_BYTES / PICO_ERASE_PAGE_SIZE)
 #define PICO_FLASH_PAGE_PER_BLOCK (PICO_ERASE_PAGE_SIZE / PICO_PROG_PAGE_SIZE)
 
 /*
@@ -15,18 +15,17 @@
  */
 
 struct ram_flash_sim {
-    struct flash_block* device_blocks[DEVICE_BLOCK_COUNT];
+    struct flash_block* device_blocks[PICO_DEVICE_BLOCK_COUNT];
 
     uint32_t base_address;
 };
 
-struct flash_page {
-    uint8_t data[PICO_PROG_PAGE_SIZE];
-};
-
-
 struct flash_block {
     struct flash_page* pages[PICO_FLASH_PAGE_PER_BLOCK];
+};
+
+struct flash_page {
+    uint8_t data[PICO_PROG_PAGE_SIZE];
 };
 
 struct ram_flash_sim device;
@@ -53,21 +52,6 @@ void dvRemoveBlock(struct ram_flash_sim* block_device, uint32_t address) {
     }
 }
 
-struct flash_fs {
-    struct ram_flash_sim* device;
-    uint32_t fs_device_offset;
-};
-
-uint32_t fsAddressForBlock(struct flash_fs* fs, uint32_t block) {
-    return block * PICO_ERASE_PAGE_SIZE + fs->device->base_address;
-}
-
-uint32_t fsAddressForBlockOff(struct flash_fs* fs, uint32_t block, uint32_t off) {
-    return fsAddressForBlock(fs, block) + off;
-}
-
-
-
 struct flash_block* getOrCreateDeviceBlock(struct ram_flash_sim* device, uint32_t address) {
     uint32_t block = getDeviceBlockNo(device, address);
 
@@ -81,50 +65,12 @@ struct flash_block* getOrCreateDeviceBlock(struct ram_flash_sim* device, uint32_
     return device->device_blocks[block];
 }
 
-
-uint32_t fsBlockAddress(struct flash_fs* fs, uint32_t block) {
-    uint32_t address = fs->device->base_address + fs->fs_device_offset + block * PICO_ERASE_PAGE_SIZE;
-    return address;
-}
-
-uint32_t fsBlockFromAddress(struct flash_fs* fs, uint32_t address) {
-    uint32_t block = address - fs->device->base_address;
-    block = block - fs->fs_device_offset;
-    block = block / PICO_ERASE_PAGE_SIZE;
-    return block;
-}
-
 void dumpBlocks(struct ram_flash_sim* block_device) {
-    for (int i = 0; i < DEVICE_BLOCK_COUNT; i++) {
+    for (int i = 0; i < PICO_DEVICE_BLOCK_COUNT; i++) {
         if (block_device->device_blocks[i]) {
             printf("Block %d: %08x\n", i, block_device->base_address + i * PICO_ERASE_PAGE_SIZE);
         }
     }
-}
-
-void removeBlock(struct ram_flash_sim* block_device, uint32_t block) {
-
-    if (block_device->device_blocks[block]) {
-        for (int i = 0; i < PICO_FLASH_PAGE_PER_BLOCK; i++) {
-            if (block_device->device_blocks[block]->pages[i]) {
-                free(block_device->device_blocks[block]->pages[i]);
-                block_device->device_blocks[block]->pages[i] = NULL;
-            }
-        }
-        free(block_device->device_blocks[block]);
-        block_device->device_blocks[block] = NULL;
-    }
-}
-
-struct flash_block* getOrCreateBlock(struct ram_flash_sim* block_device, uint32_t block) {
-    if (block_device->device_blocks[block] == NULL) {
-        block_device->device_blocks[block] = malloc(sizeof(struct flash_block));
-        for (int p = 0; p < PICO_FLASH_PAGE_PER_BLOCK; p++) {
-            block_device->device_blocks[block]->pages[p] = NULL;
-        }
-    }
-
-    return block_device->device_blocks[block];
 }
 
 struct flash_page* getOrCreatePage(struct flash_block* flash_block, uint32_t p) {
@@ -153,10 +99,30 @@ void dvInsertData(struct ram_flash_sim* block_device, uint32_t address, const ui
 
 }
 
+void dvReadData(struct ram_flash_sim* block_device, uint32_t address, void *buffer, lfs_size_t size) {
+
+    uint32_t page_offset = (address - block_device->base_address) % PICO_ERASE_PAGE_SIZE;
+
+    uint32_t prog_page = page_offset / PICO_PROG_PAGE_SIZE;
+    uint32_t byte_offset = page_offset % PICO_PROG_PAGE_SIZE;
+
+    uint32_t block = getDeviceBlockNo(block_device, address);
+
+    struct flash_block * candidate = block_device->device_blocks[block];
+    if (   candidate
+        && candidate->pages[prog_page]) {
+        printf("Read   available block %d, off %d (size: %d) as %08x, %d\n", block, page_offset, size, prog_page, byte_offset);
+        memcpy(buffer, &candidate->pages[prog_page]->data[byte_offset], size);
+    }
+    else {
+        printf("Read unavailable block %d, off %d (size: %d) as %08x, %d\n", block, page_offset, size, prog_page, byte_offset);
+    }
+}
+
 int countPages(struct ram_flash_sim* block_device) {
     int count = 0;
 
-    for (int i = 0; i < DEVICE_BLOCK_COUNT; i++) {
+    for (int i = 0; i < PICO_DEVICE_BLOCK_COUNT; i++) {
         if (block_device->device_blocks[i]) {
             for (int p = 0; p < PICO_FLASH_PAGE_PER_BLOCK; p++) {
                 if (block_device->device_blocks[i]->pages[p]) {
@@ -174,7 +140,7 @@ bool writeToFile(struct ram_flash_sim* device, FILE* output) {
 
     int cursor = 0;
 
-    for (int i = 0; i < DEVICE_BLOCK_COUNT; i++) {
+    for (int i = 0; i < PICO_DEVICE_BLOCK_COUNT; i++) {
         if (device->device_blocks[i]) {
             for (int p = 0; p < PICO_FLASH_PAGE_PER_BLOCK; p++) {
                 if (device->device_blocks[i]->pages[p]) {
@@ -225,11 +191,22 @@ bool readFromFile(struct ram_flash_sim* device, FILE* input) {
     return true;
 }
 
+struct flash_fs {
+    struct ram_flash_sim* device;
+    uint32_t fs_device_offset;
+};
+
+uint32_t fsAddressForBlock(struct flash_fs* fs, uint32_t block, uint32_t off) {
+
+    uint32_t byte_offset = block * PICO_ERASE_PAGE_SIZE + off;
+
+    return fs->device->base_address + fs->fs_device_offset + byte_offset;
+}
 
 struct ram_flash_sim* uf2_hal_init(uint32_t flash_base_address) {
 
     device.base_address = flash_base_address;
-    for (int i = 0; i < DEVICE_BLOCK_COUNT; i++) {
+    for (int i = 0; i < PICO_DEVICE_BLOCK_COUNT; i++) {
         device.device_blocks[i] = NULL;
     }
 
@@ -258,27 +235,18 @@ int uf2_read_flash_block(const struct lfs_config *c, lfs_block_t block, lfs_off_
 
     struct flash_fs* fs = c->context;
 
-    uint32_t prog_page = off / PICO_PROG_PAGE_SIZE;
-    uint32_t page_offset = off % PICO_PROG_PAGE_SIZE;
+    uint32_t device_address = fsAddressForBlock(fs, block, off);
 
-    struct flash_block * candidate = fs->device->device_blocks[block];
-    if (   candidate
-        && candidate->pages[prog_page]) {
-        printf("Read   available block %d, off %d (size: %d) as %08x, %d\n", block, off, size, prog_page, page_offset);
-        memcpy(buffer, &candidate->pages[prog_page]->data[page_offset], size);
-        return LFS_ERR_OK;
-    }
-    else {
-        printf("Read unavailable block %d, off %d (size: %d) as %08x, %d\n", block, off, size, prog_page, page_offset);
-        return LFS_ERR_OK;
-    }
+    dvReadData(fs->device, device_address, buffer, size);
+
+    return LFS_ERR_OK; 
 }
 
 int uf2_prog_flash_block(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size) {
 
     struct flash_fs* fs = c->context;
 
-    uint32_t device_address = fsAddressForBlockOff(fs, block, off);
+    uint32_t device_address = fsAddressForBlock(fs, block, off);
 
     dvInsertData(fs->device, device_address, buffer, size);
 
@@ -291,7 +259,7 @@ int uf2_erase_flash_block(const struct lfs_config *c, lfs_block_t block) {
 
     struct flash_fs* fs = c->context;
 
-    uint32_t device_address = fsAddressForBlock(fs, block);
+    uint32_t device_address = fsAddressForBlock(fs, block, 0);
 
     dvRemoveBlock(fs->device, device_address);
     
@@ -299,8 +267,6 @@ int uf2_erase_flash_block(const struct lfs_config *c, lfs_block_t block) {
 }
 
 int uf2_sync_flash_block(const struct lfs_config *c) {
-
-    struct flash_fs* fs = c->context;
 
     return LFS_ERR_OK;
 }
